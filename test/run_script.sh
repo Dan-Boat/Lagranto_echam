59a1c626235bbf1e33b70100245a56907003b4eb
Dan-Boat <dannboateng@gmail.com>
2021-12-08 15:32:59 +0100

    update

---
+++ test/run_script.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Run script using lagranto package 
+#--------------------------------------
+
+# moving climate files into the run folder
+
+
+
+# creating starting points of the trajectory (backward or forward)
+
+
+# calculating the trajectory 
+
+
+# selecting the required trajectories base on criteria (eg..within certain domain)
+
+
+# tracing certain variables along the trajectory (eg. precipitation, temperature)
+
+
+# optional (merging more than one trajectory together)
+
+
+# optional (calculating density of trajectories)
