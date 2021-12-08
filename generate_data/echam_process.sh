b4f1dc242581fdfbe8d9f34bc7f91d5495a4282a
Dan-Boat <dannboateng@gmail.com>
2021-12-08 17:13:40 +0100

    update

--- generate_data/echam_process.sh
+++ generate_data/echam_process.sh
@@ -4,14 +4,19 @@
 #--------------------------------------------------------------
 # It requires that nco is installed on your system. 
 
+#path to data 
+
+mpath='/home/dboateng/Model_output_pst/a001_hpc-bw_e5w2.3_t159_PI_Alps_east_300_t159l31.6h/output_processed/6h_MEANS'
+filename='a001_1003_1017_07_lterm.01.nc'
+
 #inverting latitudes
 
-ncpdq -O -a -lat echam_input output
+ncpdq -O -a -lat ${mpath}/${filename} ${mpath}/${filename}
 
 # rotating of longitudes 
 
-ncks -O -msa -d lon,180.,360. -d lon,0.,179. echam_input output 
+ncks -O -msa -d lon,180.,360. -d lon,0.,179. ${mpath}/${filename} ${mpath}/${filename} 
 
 # changing lats to -180 to 180
 
-ncap2 -O -s 'where(lon>=180.0) lon = lon-360.0' echam_input output
+ncap2 -O -s 'where(lon>=180.0) lon = lon-360.0' ${mpath}/${filename} ${mpath}/${filename}
